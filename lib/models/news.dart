30d1ef4a29bc68e11108c3ada7a7c073f6b0380a
davalex2003 <pupkinvasek200356@gmail.com>
2024-02-18 00:07:30 +0300

    json_serializable

--- lib/models/news.dart
+++ lib/models/news.dart
@@ -1,3 +1,8 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'news.g.dart';
+
+@JsonSerializable()
 class News {
   String title;
   String author;
@@ -7,12 +12,16 @@ class News {
   String content;
   String articleUrl;
 
-  News(
-      {required this.title,
-      required this.description,
-      required this.author,
-      required this.content,
-      required this.publishedAt,
-      required this.urlToImage,
-      required this.articleUrl});
+  News({
+    required this.title,
+    required this.author,
+    required this.description,
+    required this.urlToImage,
+    required this.publishedAt,
+    required this.content,
+    required this.articleUrl,
+  });
+
+  factory News.fromJson(Map<String, dynamic> json) => _$NewsFromJson(json);
+  Map<String, dynamic> toJson() => _$NewsToJson(this);
 }
